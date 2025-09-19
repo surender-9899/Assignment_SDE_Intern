41cecd866555fac6095ef424e8d9a120f00d659d
surender kumar mahawar <surendermahawar2@gmail.com>
2025-09-20 00:48:28 +0530

    First commit, Put the SpringBoot project on my Github repo

---
+++ src/main/java/com/Code/Inbound/Assignment_SDE_Intern/AssignmentSdeInternApplication.java
@@ -0,0 +1,13 @@
+package com.Code.Inbound.Assignment_SDE_Intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSdeInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentSdeInternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Code/Inbound/Assignment_SDE_Intern/AssignmentSdeInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Code.Inbound.Assignment_SDE_Intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSdeInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
